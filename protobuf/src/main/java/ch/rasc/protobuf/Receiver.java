e5a0fd36eac4a3e1380509df3928d802407d202a
Ralph Schaer <ralphschaer@gmail.com>
2017-01-07 07:33:08 +0100

    Add protobuf example

---
+++ protobuf/src/main/java/ch/rasc/protobuf/Receiver.java
@@ -0,0 +1,51 @@
+package ch.rasc.protobuf;
+
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.ip.dsl.Udp;
+import org.springframework.messaging.Message;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import ch.rasc.protobuf.SensorMessageOuterClass.SensorMessage;
+
+@Configuration
+@EnableIntegration
+public class Receiver {
+
+	public static void main(String[] args) throws InterruptedException {
+		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(
+				Receiver.class)) {
+			TimeUnit.MINUTES.sleep(2);
+		}
+	}
+
+	@Bean
+	public IntegrationFlow flow() {
+		return IntegrationFlows.from(Udp.inboundAdapter(9992))
+				.transform(this::transformMessage).handle(this::handleMessage).get();
+	}
+
+	private SensorMessage transformMessage(byte[] payload) {
+		try {
+			return SensorMessage.parseFrom(payload);
+		}
+		catch (InvalidProtocolBufferException e) {
+			LogFactory.getLog(Receiver.class).error("transform", e);
+			return null;
+		}
+	}
+
+	private void handleMessage(Message<?> msg) {
+		SensorMessage sensorMessage = (SensorMessage) msg.getPayload();
+		System.out.println(sensorMessage);
+	}
+
+}
