d0bafa5020dc983b0ad1ae1a445081a95d13ba73
Ralph Schaer <ralphschaer@gmail.com>
2017-03-09 07:26:23 +0100

    Upgrade to ionic 2.2.0

--- push/client/src/app/app.module.ts
+++ push/client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import {NgModule, ErrorHandler} from '@angular/core';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
-import {Storage} from "@ionic/storage";
+import {IonicStorageModule} from "@ionic/storage";
 
 @NgModule({
   declarations: [
@@ -10,14 +10,15 @@ import {Storage} from "@ionic/storage";
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
 export class AppModule {
 }
