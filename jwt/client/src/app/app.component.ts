3a21e8ac8f01df34904ba9e3b0486ac4eb927b61
Ralph Schaer <ralphschaer@gmail.com>
2017-03-24 09:11:35 +0100

    Upgrade to ionic native 3.1.0

--- jwt/client/src/app/app.component.ts
+++ jwt/client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
-import {StatusBar, Splashscreen} from 'ionic-native';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 import {HomePage} from '../pages/home/home';
 import {LoginPage} from "../pages/login/login";
 import {AuthService} from "../providers/auth-service";
@@ -12,10 +13,11 @@ export class MyApp {
   rootPage: any = null;
 
   constructor(platform: Platform,
+              statusBar: StatusBar, splashScreen: SplashScreen,
               private readonly authService: AuthService) {
     platform.ready().then(() => {
-      StatusBar.styleDefault();
-      Splashscreen.hide();
+      statusBar.styleDefault();
+      splashScreen.hide();
     });
 
     this.authService.authUser.subscribe(jwt => {
--- jwt/client/src/app/app.module.ts
+++ jwt/client/src/app/app.module.ts
@@ -2,6 +2,8 @@ import {NgModule, ErrorHandler} from '@angular/core';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {LoginPage} from "../pages/login/login";
 import {SignupPage} from "../pages/signup/signup";
 import {CustomFormsModule} from 'ng2-validation'
@@ -40,6 +42,8 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions, stor
     SignupPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
+    StatusBar,
+    SplashScreen,
     AuthService,
     JwtHelper, {
       provide: AuthHttp,
