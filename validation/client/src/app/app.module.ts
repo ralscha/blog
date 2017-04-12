a934821d3884fe4b93122613059eb097466c09d0
Ralph Schaer <ralphschaer@gmail.com>
2017-04-12 20:47:14 +0200

    Upgrade to Ionic 3

--- validation/client/src/app/app.module.ts
+++ validation/client/src/app/app.module.ts
@@ -1,8 +1,12 @@
+import {BrowserModule} from '@angular/platform-browser';
 import {NgModule, ErrorHandler} from '@angular/core';
 import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
 import {MyApp} from './app.component';
 import {HomePage} from '../pages/home/home';
 import {UsernameValidator} from "../pages/home/username-validator";
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {HttpModule} from "@angular/http";
 
 @NgModule({
   declarations: [
@@ -10,6 +14,8 @@ import {UsernameValidator} from "../pages/home/username-validator";
     HomePage
   ],
   imports: [
+    HttpModule,
+    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -17,7 +23,10 @@ import {UsernameValidator} from "../pages/home/username-validator";
     MyApp,
     HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, UsernameValidator]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, UsernameValidator]
 })
 export class AppModule {
 }
