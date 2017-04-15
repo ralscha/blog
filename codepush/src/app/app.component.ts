34eaf5abc9e7fc7134ab2f0dd8222cfd0c72d3bd
Ralph Schaer <ralphschaer@gmail.com>
2017-04-15 18:02:47 +0200

    Upgrade to Ionic 3

--- codepush/src/app/app.component.ts
+++ codepush/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {Platform} from 'ionic-angular';
-import {StatusBar, Splashscreen} from 'ionic-native';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
 import {HomePage} from '../pages/home/home';
 //import {Observable} from "rxjs";
 declare var codePush: any;
@@ -9,18 +10,21 @@ declare var codePush: any;
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(private platform: Platform,
+              private statusBar: StatusBar,
+              private splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-      // codePush.sync();
+      statusBar.styleDefault();
+      splashScreen.hide();
+
+      platform.resume.subscribe(() =>
+               codePush.sync(null, {deploymentKey: 'inbXiyN8QhcattkPFQ5mDmhSdAF4V16RCXmIG'}));
+
+      // const source = Observable.timer(5000, 1000*60*60);
+      // const subscription = source.subscribe(() => codePush.sync());
     });
 
-    platform.resume.subscribe(() => codePush.sync(null, {deploymentKey: 'b5s7XEMEPVsDhP54yK7_BHulXFg2V16RCXmIG'}));
-
-    // const source = Observable.timer(5000, 1000*60*60);
-    // const subscription = source.subscribe(() => codePush.sync());
   }
 }
--- codepush/src/app/app.module.ts
+++ codepush/src/app/app.module.ts
@@ -1,7 +1,10 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
 
 @NgModule({
   declarations: [
@@ -9,6 +12,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -16,6 +20,11 @@ import { HomePage } from '../pages/home/home';
     MyApp,
     HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}
