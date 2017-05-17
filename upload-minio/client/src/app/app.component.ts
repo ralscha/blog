7103757f9c190bf180c0c8fc2e7ec21a703f7023
Ralph Schaer <ralphschaer@gmail.com>
2017-05-17 20:40:28 +0200

    Upgrade ionic

--- upload-minio/client/src/app/app.component.ts
+++ upload-minio/client/src/app/app.component.ts
@@ -4,8 +4,6 @@ import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 
 import {HomePage} from '../pages/home/home';
-
-
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,3 +19,4 @@ export class MyApp {
     });
   }
 }
+
