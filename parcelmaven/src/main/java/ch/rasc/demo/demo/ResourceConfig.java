3112df7ce35783fd253918a494aca04f5ebbe700
Ralph Schaer <ralphschaer@gmail.com>
2018-01-30 16:16:47 +0100

    Add redirect from / to index.html

--- parcelmaven/src/main/java/ch/rasc/demo/demo/ResourceConfig.java
+++ parcelmaven/src/main/java/ch/rasc/demo/demo/ResourceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
 
@@ -29,14 +30,17 @@ class ResourceConfig implements WebMvcConfigurer {
     else {
       registry.addResourceHandler("/", "/index.html")
           .addResourceLocations("classpath:/static/")
-          .setCacheControl(CacheControl.noCache())
-          .resourceChain(false).addResolver(new GzipResourceResolver());
-      
-      registry.addResourceHandler("/**")
-          .addResourceLocations("classpath:/static/")
+          .setCacheControl(CacheControl.noCache()).resourceChain(false)
+          .addResolver(new GzipResourceResolver());
+
+      registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
           .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
           .resourceChain(false).addResolver(new GzipResourceResolver());
     }
   }
 
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/").setViewName("redirect:/index.html");
+  }
 }
