c1031a37be6c093c0409bcb4fa7ff38b3f042d58
Ralph Schaer <ralphschaer@gmail.com>
2018-02-27 18:09:44 +0100

    Reformat

--- verbalregex/src/main/java/ch/rasc/verbalregex/VerbalRegex2.java
+++ verbalregex/src/main/java/ch/rasc/verbalregex/VerbalRegex2.java
@@ -19,10 +19,18 @@ public class VerbalRegex2 {
     String input = "123xy-ab-311de";
     System.out.println(regex.test(input));
 
-    VerbalExpression.Builder part = VerbalExpression.regex().range("1", "9").count(3)
-        .range("a", "z").count(2);
-    regex = VerbalExpression.regex().startOfLine().add(part).then("-").range("a", "z")
-        .count(2).then("-").add(part).endOfLine().build();
+    VerbalExpression.Builder part = VerbalExpression.regex()
+        .range("1", "9").count(3).range("a", "z").count(2);
+    
+    regex = VerbalExpression.regex()
+        .startOfLine()
+        .add(part)
+        .then("-")
+        .range("a", "z").count(2)
+        .then("-")
+        .add(part)
+        .endOfLine()
+        .build();
 
     System.out.println(regex.test(input));
   }
