d537ae8825f7f8ac53aafbce6bb914a304c841aa
Ralph Schaer <ralphschaer@gmail.com>
2018-03-22 09:30:58 +0100

    Upgrade to Spring Boot 2

--- validation/server/src/main/java/ch/rasc/validation/Registration.java
+++ validation/server/src/main/java/ch/rasc/validation/Registration.java
@@ -4,8 +4,8 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 public class Registration {
 
