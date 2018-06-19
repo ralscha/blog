dd30a97021521b19a59e01ae74a286c9bc3bb690
Ralph Schaer <ralphschaer@gmail.com>
2018-06-19 06:47:23 +0200

    Upgrade to workbox 3.3.0

--- workbox/src/service-worker.js
+++ workbox/src/service-worker.js
@@ -1,8 +1,11 @@
-importScripts('workbox-3.2.0/workbox-sw.js');
+importScripts('workbox-3.3.0/workbox-sw.js');
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: 'workbox-3.2.0/'
+  modulePathPrefix: 'workbox-3.3.0/'
 });
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([{
+  "url": "https://static.rasc.ch/Workbox-Logo-Grey.svg"
+}]);
--- workboxlazy/src/service-worker.js
+++ workboxlazy/src/service-worker.js
@@ -1,11 +1,8 @@
-importScripts('workbox-3.2.0/workbox-sw.js');
+importScripts('workbox-3.3.0/workbox-sw.js');
 workbox.setConfig({
   debug: false,
-  modulePathPrefix: 'workbox-3.2.0/'
+  modulePathPrefix: 'workbox-3.3.0/'
 });
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
-workbox.precaching.precacheAndRoute([{
-   "url": "assets/fonts/ionicons.woff2?v=4.0.0-19"
-}]);
