64b85b9e9b148a5acfc9e3ad5f5eef393e40be71
Ralph Schaer <ralphschaer@gmail.com>
2018-06-19 06:56:32 +0200

    Fix

--- workboxlazy/src/service-worker.js
+++ workboxlazy/src/service-worker.js
@@ -6,3 +6,6 @@ workbox.setConfig({
 workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
+workbox.precaching.precacheAndRoute([{
+   "url": "assets/fonts/ionicons.woff2?v=4.0.0-19"
+}]);
