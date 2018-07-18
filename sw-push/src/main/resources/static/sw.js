b6628d534ebe3d51e3c44d8173cb04bcb64c38b0
Ralph Schaer <ralphschaer@gmail.com>
2018-07-18 08:26:06 +0200

    Upgrade libraries

--- sw-push/src/main/resources/static/sw.js
+++ sw-push/src/main/resources/static/sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.2.0/firebase-messaging.js');
 
 firebase.initializeApp({
 	'messagingSenderId': '532700130849'
