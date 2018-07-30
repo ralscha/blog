ab2e5ff4c4617994ef5b45c0abab61ecd7f5d0ba
Ralph Schaer <ralphschaer@gmail.com>
2018-07-30 21:29:48 +0200

    Upgrade libraries

--- workboxlazy/src/service-worker.js
+++ workboxlazy/src/service-worker.js
@@ -7,5 +7,5 @@ workbox.skipWaiting();
 workbox.clientsClaim();
 workbox.precaching.precacheAndRoute([]);
 workbox.precaching.precacheAndRoute([{
-   "url": "assets/fonts/ionicons.woff2?v=4.2.4"
+   "url": "assets/fonts/ionicons.woff2?v=4.2.5"
 }]);
