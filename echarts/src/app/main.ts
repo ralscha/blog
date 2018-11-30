29758c339f6d446d325af308e6dafc59e94f5a76
Ralph Schaer <ralphschaer@gmail.com>
2018-11-30 11:23:02 +0100

    Upgrade to Ionic 4

--- echarts/src/app/main.ts
+++
@@ -1,6 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-import 'echarts/theme/dark.js';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
