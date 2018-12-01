6517e95d486b7d347d443fa391a79c22514f1ae2
Ralph Schaer <ralphschaer@gmail.com>
2018-12-01 10:42:46 +0100

    Fix deprecated warnings

--- poll/server/src/main/java/ch/rasc/demo/poll/ResourceConfig.java
+++ poll/server/src/main/java/ch/rasc/demo/poll/ResourceConfig.java
@@ -6,11 +6,12 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.resource.GzipResourceResolver;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
 @Configuration
 class ResourceConfig implements WebMvcConfigurer {
@@ -20,7 +21,7 @@ class ResourceConfig implements WebMvcConfigurer {
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    if (this.environment.acceptsProfiles("development")) {
+    if (this.environment.acceptsProfiles(Profiles.of("development"))) {
       String userDir = System.getProperty("user.dir");
       registry.addResourceHandler("/**")
           .addResourceLocations(Paths.get(userDir, "../client/dist").toUri().toString())
@@ -30,11 +31,11 @@ class ResourceConfig implements WebMvcConfigurer {
       registry.addResourceHandler("/index.html")
           .addResourceLocations("classpath:/static/")
           .setCacheControl(CacheControl.noCache()).resourceChain(false)
-          .addResolver(new GzipResourceResolver());
+          .addResolver(new EncodedResourceResolver());
 
       registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
           .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS).cachePublic())
-          .resourceChain(false).addResolver(new GzipResourceResolver());
+          .resourceChain(false).addResolver(new EncodedResourceResolver());
     }
   }
 
