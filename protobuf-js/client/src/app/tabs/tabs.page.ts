1591992250c909de385b545448113c7a2ad32bb2
Ralph Schaer <ralphschaer@gmail.com>
2019-05-30 20:02:47 +0200

    Upgrade to Angular 8

--- protobuf-js/client/src/app/tabs/tabs.page.ts
+++ protobuf-js/client/src/app/tabs/tabs.page.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
-  templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss']
 })
 export class TabsPage {
 }
