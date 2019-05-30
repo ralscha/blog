26fb9d3ed905a4bbd8d6454c9bb304da9b7e7f23
Ralph Schaer <ralphschaer@gmail.com>
2019-05-30 17:14:44 +0200

    Upgrade to Angular 8

--- echarts/src/app/app-routing.module.ts
+++ echarts/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './tabs/tabs.module#TabsPageModule'}
+  {path: '', loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [
+    RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules, useHash: true})
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
