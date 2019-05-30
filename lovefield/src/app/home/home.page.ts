8cf29b3fabcdd39c34f4cec368f866d3ae3468e0
Ralph Schaer <ralphschaer@gmail.com>
2019-05-30 13:36:56 +0200

    Upgrade to Angular 8

--- lovefield/src/app/home/home.page.ts
+++ lovefield/src/app/home/home.page.ts
@@ -6,8 +6,8 @@ import {Filter} from '../filter';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage implements OnInit {
   earthquakes = [];
