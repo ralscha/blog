596c614714813be1b44ce913feda709a6be10a33
Ralph Schaer <ralphschaer@gmail.com>
2019-05-30 19:48:38 +0200

    Upgrade to Angular 8

--- secure-todo-asm/src/app/home/home.page.ts
+++ secure-todo-asm/src/app/home/home.page.ts
@@ -5,8 +5,8 @@ import {Todo} from '../todo';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage {
 
