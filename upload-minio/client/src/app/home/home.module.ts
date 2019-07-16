38648dd828e66f9763ea9ffb1bf61a6480023e37
Ralph Schaer <ralphschaer@gmail.com>
2019-07-16 06:18:04 +0200

    Upgrade to Angular 8

---
+++ upload-minio/client/src/app/home/home.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {IonicModule} from '@ionic/angular';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {HomePage} from './home.page';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HttpClientModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage
+      }
+    ])
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {
+}
