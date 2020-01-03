50bd39705f17f58d80cb317fe116efedb644ca9d
Ralph Schaer <ralphschaer@gmail.com>
2020-01-03 06:34:37 +0100

    Use Java 11 Http Client

--- dynamicpos/server/src/main/java/ch/rasc/dynamicpos/EarthquakeImporter.java
+++ dynamicpos/server/src/main/java/ch/rasc/dynamicpos/EarthquakeImporter.java
@@ -1,14 +1,17 @@
 package ch.rasc.dynamicpos;
 
+import java.io.IOException;
 import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.univocity.parsers.common.record.Record;
 import com.univocity.parsers.csv.CsvParser;
@@ -19,14 +22,10 @@ public class EarthquakeImporter {
 
 	private final ReactiveEarthquakeRepository repository;
 
-	private final WebClient client;
-
 	private final CsvParser parser;
 
 	EarthquakeImporter(ReactiveEarthquakeRepository repository) {
 		this.repository = repository;
-
-		this.client = WebClient.create("https://earthquake.usgs.gov");
 
 		CsvParserSettings settings = new CsvParserSettings();
 		settings.setHeaderExtractionEnabled(true);
@@ -35,13 +34,18 @@ public class EarthquakeImporter {
 	}
 
 	@PostConstruct
-	public void read() {
-		String importedData = this.client.get()
-				.uri("/earthquakes/feed/v1.0/summary/2.5_month.csv")
-				.accept(MediaType.TEXT_PLAIN).retrieve().bodyToMono(String.class).block();
+	public void read() throws IOException, InterruptedException {
+		HttpClient httpClient = HttpClient.newHttpClient();
+
+		HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(
+				"https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.csv"))
+				.build();
+
+		HttpResponse<String> response = httpClient.send(request,
+				HttpResponse.BodyHandlers.ofString());
 
 		List<Record> records = this.parser
-				.parseAllRecords(new StringReader(importedData));
+				.parseAllRecords(new StringReader(response.body()));
 		List<Earthquake> earthquakes = records.stream().map(Earthquake::new)
 				.collect(Collectors.toList());
 		insertData(earthquakes);
