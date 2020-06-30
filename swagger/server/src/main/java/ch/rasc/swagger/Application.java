561bda664fb7e1bdd73c6d44083292a7fb0ffa03
Ralph Schaer <ralphschaer@gmail.com>
2020-06-30 03:57:48 +0200

    Upgrade libraries

--- swagger/server/src/main/java/ch/rasc/swagger/Application.java
+++ swagger/server/src/main/java/ch/rasc/swagger/Application.java
@@ -9,10 +9,10 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class Application {
 
   public static void main(String[] args) {
