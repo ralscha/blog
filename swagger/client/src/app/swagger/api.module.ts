de51470eec8f6f42c8785c0d5cd1c9fa0319cc31
Ralph Schaer <ralphschaer@gmail.com>
2020-10-26 07:13:30 +0100

    Fix

--- swagger/client/src/app/swagger/api.module.ts
+++ swagger/client/src/app/swagger/api.module.ts
@@ -13,7 +13,7 @@ import {TodoServiceService} from './api/todoService.service';
     TodoServiceService]
 })
 export class ApiModule {
-  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
     return {
       ngModule: ApiModule,
       providers: [{provide: Configuration, useFactory: configurationFactory}]
