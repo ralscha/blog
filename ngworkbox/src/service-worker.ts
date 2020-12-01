6ae51402a13da75579721bb6ba9684eda9c69950
Ralph Schaer <ralphschaer@gmail.com>
2020-12-01 03:55:16 +0100

    Upgrade libraries

--- ngworkbox/src/service-worker.ts
+++ ngworkbox/src/service-worker.ts
@@ -1,13 +1,13 @@
 /// <reference lib="es2018" />
 /// <reference lib="webworker" />
 import {precacheAndRoute} from 'workbox-precaching';
-import {clientsClaim, skipWaiting} from 'workbox-core';
+import {clientsClaim} from 'workbox-core';
 import {registerRoute} from 'workbox-routing';
 import {CacheFirst} from 'workbox-strategies';
 
 declare const self: ServiceWorkerGlobalScope;
 
-skipWaiting();
+self.skipWaiting();
 clientsClaim();
 
 if (process.env.NODE_ENV === 'production') {
