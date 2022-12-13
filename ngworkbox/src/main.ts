c4fb81a8b697134e9007b2696eea61aa6aa958b7
Ralph Schaer <ralphschaer@gmail.com>
2022-12-13 19:02:29 +0100

    Update main.ts

--- ngworkbox/src/main.ts
+++ ngworkbox/src/main.ts
@@ -1,7 +1,39 @@
+import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
 import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import {Workbox} from 'workbox-window';
 
+function loadServiceWorker(): void {
+  // if (environment.production && ('serviceWorker' in navigator)) {
+  if ('serviceWorker' in navigator) {
+    const wb = new Workbox('service-worker.js');
+
+    wb.addEventListener('activated', (event) => {
+      if (!event.isUpdate) {
+        console.log('Service worker activated for the first time!');
+      } else {
+        console.log('Service worker activated!');
+      }
+    });
+
+    wb.addEventListener('waiting', (event) => {
+      console.log(`A new service worker has installed, but it can't activate` +
+        `until all tabs running the current version have fully unloaded.`);
+    });
+
+    wb.addEventListener('installed', (event) => {
+      if (!event.isUpdate) {
+        console.log('Service worker installed for the first time');
+      } else {
+        console.log('Service worker installed');
+      }
+    });
+
+    wb.register();
+  }
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .then(_ => loadServiceWorker())
   .catch(err => console.error(err));
