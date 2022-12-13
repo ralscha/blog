60ea97327d28a524aa92ffe3d2e6770a21eb6167
Ralph Schaer <ralphschaer@gmail.com>
2022-12-13 18:58:51 +0100

    Update main-simple.ts

--- ngworkbox/src/main-simple.ts
+++ ngworkbox/src/main-simple.ts
@@ -3,10 +3,6 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app/app.module';
 import {environment} from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
-}
-
 function loadServiceWorker(): void {
   // if (environment.production && ('serviceWorker' in navigator)) {
   if ('serviceWorker' in navigator) {
