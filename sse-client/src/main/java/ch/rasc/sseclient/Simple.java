1564ba89fff80a7a5e9b7773b9fab30704699e01
Ralph Schaer <ralphschaer@gmail.com>
2026-08-29 06:14:40 +0200

    Upgrade

--- sse-client/src/main/java/ch/rasc/sseclient/Simple.java
+++ sse-client/src/main/java/ch/rasc/sseclient/Simple.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.concurrent.TimeUnit;
 
 import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.RetryDelayStrategy;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import com.launchdarkly.eventsource.background.BackgroundEventSource;
 
@@ -12,8 +13,9 @@ public class Simple {
   public static void main(String[] args) throws InterruptedException {
     BackgroundEventHandler eventHandler = new SimpleEventHandler();
     String url = String.format("http://localhost:8080/memory");
-    BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler, 
-        new EventSource.Builder(URI.create(url)).retryDelay(3, TimeUnit.SECONDS));
+    BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler,
+        new EventSource.Builder(URI.create(url)).retryDelayStrategy(
+            RetryDelayStrategy.defaultStrategy().initialDelay(3, TimeUnit.SECONDS)));
 
     try (BackgroundEventSource eventSource = builder.build()) {
       eventSource.start();
